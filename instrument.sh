76c5d902e252af6c2f2ac5b3134b6b2eefac6b11
StevenShen3641 <stevenshen3641@outlook.com>
2025-02-04 08:16:07 +0000

    update

--- instrument.sh
+++ instrument.sh
@@ -11,7 +11,6 @@ set -e
 ##
 
 aflgo_patch_file="$FUZZER/src/aflgo.patch"
-
 # openssl
 if [ "$(basename $TARGET)" == "openssl" ]; then
     echo "TARGET openssl"
@@ -30,19 +29,18 @@ export TMP_DIR=$OUT/temp
 export CC=$AFLGO/afl-clang-fast
 export CXX=$AFLGO/afl-clang-fast++
 
-(	
-	echo "## Set Target"
+(
+    echo "## Set Target"
     pushd $TARGET/repo
-	echo "## Get Target"
-	echo "targets"
+    echo "## Get Target"
+    echo "targets"
 
-	# Need starting position
-	grep -nr MAGMA_LOG | cut -f1,2 -d':' | grep -v ".orig:"  | grep -v "Binary file" > $TMP_DIR/real.txt
+    # Need starting position
+    grep -nr MAGMA_LOG | cut -f1,2 -d':' | grep -v ".orig:" | grep -v "Binary file" >$TMP_DIR/real.txt
 
-	cat $TMP_DIR/real.txt
-	popd
+    cat $TMP_DIR/real.txt
+    popd
 )
-
 
 export LDFLAGS="$LDFLAGS -lpthread"
 export ADDITIONAL="-targets=$TMP_DIR/BBtargets.txt -outdir=$TMP_DIR -flto -fuse-ld=gold -Wl,-plugin-opt=save-temps"
@@ -56,29 +54,69 @@ export LIBS="$LIBS -l:afl_driver.o -lstdc++"
 TEMP_CFLAGS=$CFLAGS
 TEMP_CXXFLAGS=$CXXFLAGS
 
-if [ "$(basename $TARGET)" == "openssl" ]; then
+case "$(basename $TARGET)" in
+"openssl")
     echo "TARGET openssl"
-	export CONFIGURE_FLAGS="$ADDITIONAL"
-else
+    CONFIGURE_FLAGS="$ADDITIONAL"
+    ;;
+"lua")
+    LDFLAGS="$LDFLAGS -flto"
     CFLAGS="$TEMP_CFLAGS $ADDITIONAL"
-	CXXFLAGS="$TEMP_CXXFLAGS $ADDITIONAL"
-fi
+    CXXFLAGS="$TEMP_CXXFLAGS $ADDITIONAL"
+    ;;
+*)
+    CFLAGS="$TEMP_CFLAGS $ADDITIONAL"
+    CXXFLAGS="$TEMP_CXXFLAGS $ADDITIONAL"
+    ;;
+esac
 
 "$TARGET/build.sh"
 
-cat $TMP_DIR/BBnames.txt | rev | cut -d: -f2- | rev | sort | uniq > $TMP_DIR/BBnames2.txt && mv $TMP_DIR/BBnames2.txt $TMP_DIR/BBnames.txt
-cat $TMP_DIR/BBcalls.txt | sort | uniq > $TMP_DIR/BBcalls2.txt && mv $TMP_DIR/BBcalls2.txt $TMP_DIR/BBcalls.txt
+(
+    pushd $TARGET/repo
+
+    case "$(basename $TARGET)" in
+    "libsndfile")
+        cp ossfuzz/sndfile_fuzzer* $OUT/
+        ;;
+    "libtiff")
+        cp tools/tiffcp* $OUT/
+        ;;
+    "libxml2")
+        cp xmllint* $OUT/
+        ;;
+    "lua")
+        CC=$TEMP_CC
+        cp lua* $OUT/
+        ;;
+    "openssl")
+        fuzzers=$(find fuzz -executable -type f '!' -name \*.py '!' -name \*-test '!' -name \*.pl)
+        for f in $fuzzers; do
+            cp $f* $OUT/
+        done
+        ;;
+    "php")
+        fuzzers="php-fuzz-json php-fuzz-exif php-fuzz-mbstring php-fuzz-unserialize php-fuzz-parser"
+        for f in $fuzzers; do
+            cp sapi/fuzzer/$f* "$OUT/${f/php-fuzz-/}"
+        done
+        ;;
+    "poppler")
+        cp "$WORK/poppler/utils/"{pdfimages*,pdftoppm*} $OUT/
+        ;;
+    *)
+        echo "$(basename $TARGET)"
+        ;;
+    esac
+    popd
+)
+
+cat $TMP_DIR/BBnames.txt | rev | cut -d: -f2- | rev | sort | uniq >$TMP_DIR/BBnames2.txt && mv $TMP_DIR/BBnames2.txt $TMP_DIR/BBnames.txt
+cat $TMP_DIR/BBcalls.txt | sort | uniq >$TMP_DIR/BBcalls2.txt && mv $TMP_DIR/BBcalls2.txt $TMP_DIR/BBcalls.txt
 
 $AFLGO/scripts/genDistance.sh $OUT $TMP_DIR
 
-
-if [ "$(basename $TARGET)" == "openssl" ]; then
-    echo "TARGET openssl"
-	CONFIGURE_FLAGS="-distance=$TMP_DIR/distance.cfg.txt"
-else
-	CFLAGS="$TEMP_CFLAGS -distance=$TMP_DIR/distance.cfg.txt" CXXFLAGS="$TEMP_CXXFLAGS -distance=$TMP_DIR/distance.cfg.txt"
-fi
-
+CFLAGS="$TEMP_CFLAGS -distance=$TMP_DIR/distance.cfg.txt" CXXFLAGS="$TEMP_CXXFLAGS -distance=$TMP_DIR/distance.cfg.txt"
 "$TARGET/build.sh"
 
 # NOTE: We pass $OUT directly to the target build.sh script, since the artifact
